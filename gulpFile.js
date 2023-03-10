673f797c7ee762f33ec7a51aa7024710bbe0147e
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-03-10 15:35:27 -0300

    hero

--- gulpFile.js
+++ gulpFile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'))
+const sass = require('gulp-sass')(require('sass'));
+const imagemin  = require('gulp-imagemin');
 
 function styles(){
     console.log("Função Styles");
@@ -8,7 +9,14 @@ function styles(){
             .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images(){
+    console.log("Função Styles");
+    return  gulp.src('./src/shared/images/**/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('./dist/shared/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
 
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
