98879a112a608d419f113c50132f60269511d856
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-03-17 17:25:00 -0300

    section faq

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
+
 
     for (let i = 0 ; i < buttons.length ; i++){
 
         buttons[i].addEventListener('click', function(botao){
+
             const abaalvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaalvo}]`);
             escondeTodasAbas();
@@ -12,7 +15,17 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
 })
+
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBUttaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
