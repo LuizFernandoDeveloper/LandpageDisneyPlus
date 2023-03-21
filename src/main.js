8d2951b64ff0c4396c171814de5e43831e9006cf
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-03-20 21:48:55 -0300

    Finish header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll('[data-faq-question]')
+    const questions = document.querySelectorAll('[data-faq-question]');
+    
+    const heroSection = document.querySelector('.hero');
+    window.addEventListener('scroll', function(){
+
+        if(heroSection.clientHeight > window.scrollY ){
+            ocultaElementosDoHeader();
+        }
+        else{
+            exibeElementoDoHeader();
+        }
+    })
 
 
     for (let i = 0 ; i < buttons.length ; i++){
@@ -20,6 +31,16 @@ document.addEventListener('DOMContentLoaded', function(){
         questions[i].addEventListener('click', abreOuFechaResposta)
     }
 })
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementoDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function abreOuFechaResposta(elemento){
     const classe = 'faq__questions__item--is-open';
